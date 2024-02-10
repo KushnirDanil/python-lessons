ff4bcb8e3c423998728060896ea36836906d71c6
KushnirDanil <dankusnir09@gmail.com>
2024-02-10 14:19:44 +0200

    lessons2

---
+++ p02/exercise1.py
@@ -0,0 +1,42 @@
+import sys
+import datetime as dt
+
+# print('Area of circle')
+# radius = int(input(' input radius:'))
+# pi = 3.1415
+# area = pi * radius ** 2
+# print('Area of the circle = ', area)
+
+# print('Volume of sphere')
+# radius = int(input(' input radius:'))
+# pi = 3.141592
+# volume = 4 / 3 * pi * radius ** 3
+# print(f'Volume of the sphere: {volume}')
+
+# n = input(' input an integer: ')
+# n1 = n
+# n2 = n + n
+# n3 = n + n + n
+# # TODO: знайти суму
+# sum = int(n1) + int(n2) + int (n3)       import datetime as dt
+# print(sum)
+
+# n = int(input(' input an integer: '))
+# n1 = int("%s" % n)
+# n2 = int("%s%s" % (n, n))
+# n3 = int("%s%s%s" % (n, n, n))
+# print(n1 + n2 + n3)
+
+# print('python version')
+# print(sys.version)
+# print("Version info")
+# print(sys.version_info)
+
+# Display the current date and time
+# now = dt.datetime.now()
+# print(now.strftime('%d-%m-%Y %H:%M:%S'))
+
+# UNICODE
+# print('\x1b[0;36m\u2591\x1b[0;37m'*40)
+# print('\u259a'*13, '\x1b[4;31mUnicode text\x1b[0;37m', '\u259a'*13)
+# print('\x1b[4;30m\u2593'*40)
